23e3946122545681f67104e022b0e5474c968543
Yassine Rassy <yassine.rassy1@gmail.com>
2021-07-02 19:34:34 +0200

    create app & homepage & configuration for routes with hashHistory

--- client/index.js
+++ client/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import { ReactDOM } from "react-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from 'react-apollo';
+import {Route,Router,IndexRoute,hashHistory} from "react-router";
+import App from "./app";
+import HomePage from "./pages/home";
+//to bind apollo with react 
 const client = new ApolloClient({});
 
 const Root = () => {
     return (
         <ApolloProvider client={client}>
-            <div>Welcome to Lyrical App </div>
+            <Router history={hashHistory}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={HomePage} />
+                </Route>
+            </Router>
         </ApolloProvider>
     )
 }
--- server/schemas/root-query-type.js
+++ server/schemas/root-query-type.js
@@ -2,6 +2,7 @@ const graphql = require("graphql");
 const LyricType = require('./types/lyric')
 const SongType = require('./types/song')
 const LanguageType = require('./types/language')
+
 const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
