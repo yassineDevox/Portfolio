d3c42ddc6df25483c375d49936bb7dbcfa64b4d0
Yassine Rassy <yassine.rassy1@gmail.com>
2021-07-02 21:34:07 +0200

    design portfolio in progress ...

--- client/app.js
+++ client/app.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const App = ({children}) => {
+const App = ({ children }) => {
     return (
-        <div className="container">
-            {children}
-        </div>
+        <>
+            {/* <!-- Header Part --> */}
+            <header>
+                <div className="menu">
+                    <p className="logo">
+                        web<span>dev</span>
+                    </p>
+                    <img 
+                        className="menu-icon" 
+                        src="img/menu.svg" 
+                        alt="menu-icon"
+                    />
+                </div>
+            </header>
+
+            {/* <!-- Header Part /--> */}
+
+            {/* <!-- Main Content --> */}
+            <main className="main-content">
+                {children}
+            </main>
+
+            {/* <!-- Main Content /--> */}
+        </>
     )
 }
 export default App;
--- client/pages/home.js
+++ client/pages/home.js
@@ -8,10 +8,7 @@ export default class HomePage extends Component {
 
     render() {
         return (
-            <>
-                
-
-            </>
+            <></>
         )
     }
 
